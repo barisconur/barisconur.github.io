d1d92dc8b585db5d4f6b7748277b2abd29e90723
barisconur <barisconur@gmail.com>
2019-12-15 21:18:59 +0300

    add medium icon to social follow component

--- src/js/Components/SocialFollow.js
+++ src/js/Components/SocialFollow.js
@@ -2,20 +2,23 @@
 import React, {Fragment} from 'react';
 import '../../sass/HomePage.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faTwitter, faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons"
+import { faTwitter, faLinkedin, faGithubSquare, faMedium } from "@fortawesome/free-brands-svg-icons"
 import about from '../../json/aboutMe';
 
 export default function SocialFollow() {
   return (
     <>
       <a href={about.URL[0]} rel="noopener noreferrer" target="_blank" className="twitter social">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
+        <FontAwesomeIcon icon={faTwitter} size="2x"/>
       </a>
       <a href={about.URL[1]} rel="noopener noreferrer" target="_blank" className="github social">
-        <FontAwesomeIcon icon={faGithubSquare} size="2x" />
+        <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
       </a>
       <a href={about.URL[2]} rel="noopener noreferrer" target="_blank"className="linkedIn social">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+      </a>
+      <a href={about.URL[3]} rel="noopener noreferrer" target="_blank" className="medium social">
+        <FontAwesomeIcon icon={faMedium} size="2x"/>
       </a>
     </>
   )
