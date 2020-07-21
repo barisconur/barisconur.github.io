5557b44dbbe5cf16b456ac52d00ec7e6eb0e89b2
barisconur <barisconur@gmail.com>
2020-07-21 13:24:25 +0300

    Updates

---
+++ precache-manifest.04428307adac7cc6a47e518f79bc1090.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6002105479689fd2af41e4dfdd34e6",
+    "url": "/Online-Portfolio/index.html"
+  },
+  {
+    "revision": "b9542ded13adf0fd9e3f",
+    "url": "/Online-Portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "0a20fed68820f13ca8dc",
+    "url": "/Online-Portfolio/static/css/main.d3c871ae.chunk.css"
+  },
+  {
+    "revision": "b9542ded13adf0fd9e3f",
+    "url": "/Online-Portfolio/static/js/2.c895bb00.chunk.js"
+  },
+  {
+    "revision": "0a20fed68820f13ca8dc",
+    "url": "/Online-Portfolio/static/js/main.32a99839.chunk.js"
+  },
+  {
+    "revision": "5068cdcf7c4e1299aa3d",
+    "url": "/Online-Portfolio/static/js/runtime-main.49f5e384.js"
+  },
+  {
+    "revision": "379d5a492f91ebc6ddff9c4c89debeb9",
+    "url": "/Online-Portfolio/static/media/background.379d5a49.jpg"
+  },
+  {
+    "revision": "6c1709b01c875b45eb5c746780d559a3",
+    "url": "/Online-Portfolio/static/media/location-sign.6c1709b0.svg"
+  },
+  {
+    "revision": "649d52b0b1eb04d0c264ab85b10e1d46",
+    "url": "/Online-Portfolio/static/media/profile-photo.649d52b0.jpeg"
+  }
+]);
