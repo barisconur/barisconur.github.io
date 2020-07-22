a8362a6fdaa02976d2551f0c22937e62e1103fe0
barisconur <barisconur@gmail.com>
2020-07-23 01:39:53 +0300

    Updates

---
+++ precache-manifest.99464bb13e07e097ed2216afd858fa60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7869844eb21c77465579c19ed4674d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a73d78115a9dfd49558",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8de501edee93aceaca53",
+    "url": "/static/css/main.13fc669b.chunk.css"
+  },
+  {
+    "revision": "9a73d78115a9dfd49558",
+    "url": "/static/js/2.c95496ae.chunk.js"
+  },
+  {
+    "revision": "8de501edee93aceaca53",
+    "url": "/static/js/main.a2351323.chunk.js"
+  },
+  {
+    "revision": "9ee78bf23245eacba360",
+    "url": "/static/js/runtime-main.f8e9f13b.js"
+  },
+  {
+    "revision": "379d5a492f91ebc6ddff9c4c89debeb9",
+    "url": "/static/media/background.379d5a49.jpg"
+  },
+  {
+    "revision": "6c1709b01c875b45eb5c746780d559a3",
+    "url": "/static/media/location-sign.6c1709b0.svg"
+  },
+  {
+    "revision": "649d52b0b1eb04d0c264ab85b10e1d46",
+    "url": "/static/media/profile-photo.649d52b0.jpeg"
+  }
+]);
