46f137c9fd471397390e4ddedd8702a421ad66e2
barisconur <barisconur@gmail.com>
2020-10-14 23:48:37 +0300

    add not found page and update routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, HashRouter as Router} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Redirect, Switch} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/home/HomePage.js'; 
 import BlogPage from './pages/blog/BlogPage.js';
+import NotFoundPage from './pages/404/NotFoundPage.js';
 
 const routing = (
   <Router>
-    <div>
+    <Switch>
       <Route exact path="/" component={HomePage}/>
       <Route path="/blog" component={BlogPage}/>
-    </div>
+      <Route path="/404" component={NotFoundPage} />
+      <Redirect to="/404" />
+    </Switch>
   </Router>
 )
 
---
+++ src/pages/404/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+  <>
+    <h1>404 - This is not a valid path</h1>
+  </>
+  )
+}
